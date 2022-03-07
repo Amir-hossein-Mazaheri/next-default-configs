b82f48937bf00e592df4e1929840b0d749e7ff6c
LightSpeedGuy <uplayggboy@gmail.com>
2022-03-07 17:46:06 +0330

    added redux support

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,14 @@
 import "../styles/globals.css";
+import { Provider } from "react-redux";
+import store from "../Store/configStore";
+
 // refresh webpack server if you changed the YourAppName
 function YourAppName({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
 
 export default YourAppName;
